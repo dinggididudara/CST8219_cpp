99b1a3ebe9b69e79d3eaef6f26b1864b5cd318bc
Soomin Lee <dinggididududu@gmail.com>
2023-01-10 10:11:15 -0500

    init

---
+++ Week1/Week1/Week1.cpp
@@ -0,0 +1,24 @@
+﻿// Week1.cpp : Defines the entry point for the application.
+//
+
+#include "Week1.h"
+
+#define usingNamespaces 0
+
+#if using namespace std
+#pragma message("Using namespaces")
+int main()
+{
+	cout << "Hello world!" << endl;
+	return 0;
+}
+
+#elif ??
+#pragma message("Not using namespaces")
+int main()
+{
+	std::count << "Hellow world!" << std::endl;
+	return 0;
+}
+#endif
+
