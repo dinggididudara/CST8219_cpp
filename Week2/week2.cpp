aa9e11a5e5e28885b303ffdd2e8e1d72cf9345f1
Soomin Lee <dinggididududu@gmail.com>
2023-01-30 00:59:04 -0500

    Finished Week 3

--- Week2/week2.cpp
+++ Week2/week2.cpp
@@ -21,14 +21,47 @@ namespace CST8219{
 		Car() : Car(4) {	//empty constructor, calling another constructor
 			cout << "In constructor with 0 parameters" << endl;
 		} 
-		~Car(){} //destructor
+		~Car(){
+			cout << "In destructor" << endl;
+		} //destructor
 	}; //class end
 } //CST8219 end
 
 
 int main(int argc, char **argv)
 {
-	CST8219::Car myVehicle(4,2);
-   cout << "I made a Car!" << endl;
+   CST8219::Car veh1;
+   CST8219::Car veh2(4);
+   CST8219::Car veh3(4, 2);
+   
+   cout << "Vehicle takes " << sizeof(veh1) << endl;
+   cout << "Vehicle takes " << sizeof(veh2) << endl;
+   cout << "Vehicle takes " << sizeof(veh3) << endl;
+
+   CST8219::Car* pVehicle;
+   int d, w = 0;
+   char c;
+   do {
+	   cout << "do you want to continue? (q/Q for quit)" << endl;
+	   cin >> c;
+	   pVehicle = 0;
+		if (c == 'q' || c == 'Q') {
+			delete pVehicle;
+		   return 0;
+		}
+		else {
+			do {
+				
+				cin.clear();
+				cin.ignore(256, '\n');
+				cout << "enter number of doors" << endl;
+				cin >> d;
+				cout << "enter number of wheels" << endl;
+				cin >> w;
+			} while (d < 0 || w < 0);
+			pVehicle = new CST8219::Car(w, d);
+		}
+   } while (c != 'q' || c != 'Q');
+   
    return 0;
 }
